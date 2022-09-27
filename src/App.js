8139c7cc8a6b9a4dcb879cea225d69b318117b83
guruias123 <74144270+guruias123@users.noreply.github.com>
2022-09-27 14:28:01 +0530

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Routes,
+  Redirect,
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Home_01 from "./components/Home_01";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/home" element={<Home_01 />} />
+        </Routes>
+      </BrowserRouter>
+
+    {/* <Home /> */}
     </div>
   );
 }
